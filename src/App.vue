158fa933a2840a38d0a40894d8c6d50225f94497
josimara-lima2 <josimara_lima88@hotmail.com>
2022-09-22 10:44:07 -0300

    criando todos

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ import TodoSpinner from "./components/TodoSpinner.vue";
 import TodoFormAdd from "./components/TodoFormAdd.vue";
 import TodoItems from "./components/TodoItems.vue";
 import TodoEmpty from "./components/TodoEmpty.vue";
-import axios from "axios";
+
 export default {
   name: "App",
   components: { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty },
@@ -31,15 +31,10 @@ export default {
     };
   },
   created() {
-    this.loading = true;
-    axios
-      .get("http://localhost:3000/todos")
-      .then((response) => {
-        this.$store.commit("storeTodos", response.data);
-      })
-      .finally(() => {
-        this.loading = false;
-      });
+    this.loading = true
+    this.$store.dispatch('getTodos').finally(() => {
+      this.loading = false
+    })
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'vuex'
-
+import axios from "axios";
 export default createStore({
   state: {
     todos:[]
@@ -9,9 +9,28 @@ export default createStore({
   mutations: {
     storeTodos(state,payload){
       state.todos = payload
-    }
+    },
+    storeTodo(state,payload){
+      state.todos.push(payload)
+    },
   },
   actions: {
+    getTodos({commit}){
+      return   axios
+      .get("http://localhost:3000/todos")
+      .then((response) => {
+        commit("storeTodos", response.data);
+      })
+     
+    },
+
+    addTodo({commit},data){
+     return axios
+      .post("http://localhost:3000/todos",data).then(response => {
+        commit("storeTodo", response.data);
+      })
+    
+    }
   },
   modules: {
   }
