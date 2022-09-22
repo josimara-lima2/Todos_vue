bd45b0425cf156a4b8843fb7ef84690e0fdb2efe
josimara-lima2 <josimara_lima88@hotmail.com>
2022-09-22 10:21:49 -0300

    separação do codigo emcomponentes e listagem das todos

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="space-y-2">
+        <TodoItem :todo="todo"
+            v-for="todo in $store.state.todos" 
+            :key="todo.id">
+        </TodoItem>
+ 
+      </div>
+</template>
+
+
+<script>
+import TodoItem from './TodoItem.vue';
+export default {
+    components: { TodoItem }
+}
+</script>
